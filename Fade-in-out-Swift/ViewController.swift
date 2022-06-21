b7243c1e64e5b3a17a383540a0246f8a165d17d8
Luccas <mathaluc2016@outlook.com>
2022-06-21 11:32:56 -0300

    commit

--- Fade-in-out-Swift/ViewController.swift
+++ Fade-in-out-Swift/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemCyan
         
         self.view.addSubview(image)
+        
         fadeInOut()
         
         NSLayoutConstraint.activate([
@@ -27,13 +28,34 @@ class ViewController: UIViewController {
     lazy var image: UIImageView = {
         let img = UIImageView()
         img.translatesAutoresizingMaskIntoConstraints = false
-        img.image = UIImage(named: "pato")
+        img.contentMode = .scaleAspectFill
         return img
     }()
     
     func fadeInOut() {
         UIView.animate(withDuration: 0.6, animations: {
             self.image.alpha = 1
+            
+            let imagesArray: [UIImage?] = [
+                UIImage(named: "pato"),
+                UIImage(named: "tigre"),
+                UIImage(named: "gaviao"),
+            ]
+            
+            var myImages: UIImage?
+            var numeroSequencial = 0
+    
+//            for x in 0...2 {
+//                self.image.image = self.imagesArray[x]
+//            }
+            
+            if numeroSequencial == imagesArray.count {
+                numeroSequencial = 0
+            }
+            
+            myImages = imagesArray[numeroSequencial]
+            numeroSequencial += 1
+            
         }, completion: {
             (completed: Bool) -> Void in
             UIView.animate(withDuration: 0.6, delay: 0.6, options: UIView.AnimationOptions.curveLinear,
